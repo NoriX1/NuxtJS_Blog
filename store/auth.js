35d17bb99596528b968366a5473a777638214ae0
Dmitry Nikonenko <versatility.95@mail.ru>
2020-12-15 18:03:29 +0300

    Обработка и отображение ошибок авторизации.

--- store/auth.js
+++ store/auth.js
@@ -13,11 +13,17 @@ export const mutations = {
 
 export const actions = {
   async login({ commit, dispatch }, formData) {
-    const token = await new Promise((resolve) => {
-      setTimeout(() => resolve('mock-token'), 2000);
-    });
-    // dispatch вызывает именно actions
-    dispatch('setToken', token);
+    try {
+      const token = await new Promise((resolve, reject) => {
+        setTimeout(() => resolve('mock-token'), 2000);
+      });
+      // dispatch вызывает именно actions
+      dispatch('setToken', token);
+    } catch (e) {
+      // 3-й параметр -> смотреть мутацию в корне папки (store)
+      commit('setError', e, { root: true });
+      throw e;
+    }
   },
   setToken({ commit }, token) {
     // commit вызывает мутации (setToken в данном случае)
