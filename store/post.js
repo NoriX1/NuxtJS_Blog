8194caf27821fd35c45ebc56b7d01b99ea44286d
Dmitry Nikonenko <versatility.95@mail.ru>
2020-12-17 17:42:08 +0300

    Создание и редактирование постов

--- store/post.js
+++ store/post.js
@@ -1,35 +1,27 @@
-const posts = [
-  {
-    title: 'Post 1',
-    date: new Date(),
-    views: 22,
-    comments: [1, 2],
-    _id: '1',
-  },
-  {
-    title: 'Post 2',
-    date: new Date(),
-    views: 22,
-    comments: [1, 2],
-    _id: '2',
-  },
-];
 export const actions = {
-  // eslint-disable-next-line no-empty-pattern
-  async fetchAdminPosts({}) {
-    return await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(posts);
-      }, 1000);
-    });
+  async fetchAdminPosts({ commit }) {
+    try {
+      return await this.$axios.$get('/api/post/admin');
+    } catch (e) {
+      commit('setError', e, { root: true });
+      throw e;
+    }
   },
-  // eslint-disable-next-line no-empty-pattern
-  async remove({}, id) {
-    await console.log('remove', id);
+  async remove({ commit }, id) {
+    try {
+      return await this.$axios.$delete(`/api/post/admin/${id}`);
+    } catch (e) {
+      commit('setError', e, { root: true });
+      throw e;
+    }
   },
-  // eslint-disable-next-line no-empty-pattern
-  async update({}, { id, text }) {
-    await console.log('update', id, text);
+  async update({ commit }, { id, text }) {
+    try {
+      return await this.$axios.$put(`/api/post/admin/${id}`, { text });
+    } catch (e) {
+      commit('setError', e, { root: true });
+      throw e;
+    }
   },
   async create({ commit }, { title, text, image }) {
     try {
@@ -39,22 +31,18 @@ export const actions = {
       fd.append('text', text);
       fd.append('image', image, image.name);
 
-      return await new Promise((resolve) => {
-        setTimeout(() => {
-          resolve();
-        }, 1000);
-      });
+      return await this.$axios.$post('/api/post/admin', fd);
     } catch (e) {
       commit('setError', e, { root: true });
       throw e;
     }
   },
-  // eslint-disable-next-line no-empty-pattern
-  async fetchAdminById({}, id) {
-    return await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(posts.find((p) => p._id === id));
-      }, 1000);
-    });
+  async fetchAdminById({ commit }, id) {
+    try {
+      return await this.$axios.$get(`/api/post/admin/${id}`);
+    } catch (e) {
+      commit('setError', e, { root: true });
+      throw e;
+    }
   },
 };
